f389e0a0843d2580ac2d51bc0d3247309d8f0921
Sam Morrison <sam@vpac.org>
2012-02-10 15:51:39 +1100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,35 @@
+class mysql {
+  
+  package { 'mysql-server':
+    ensure => installed,
+  }
+  
+  service { 'mysql':
+    ensure  => running,
+    require => Package[mysql-server],
+  }
+
+  exec { 'set-mysql-root-password':
+    unless => "mysqladmin -uroot -p${mysql_root_password} status",
+    path => ["/bin", "/usr/bin"],
+    command => "mysqladmin -uroot password ${mysql_root_password}",
+    require => Service["mysql"],
+  }
+
+  define mysqldb( $user, $password ) {
+    exec { "create-${name}-db":
+      command => "mysql -u root -p${mysql_root_password} -e \"CREATE DATABASE ${name};\"",
+      creates => "/var/lib/mysql/${name}/",
+      path    => "/bin:/usr/bin",
+      require => Service["mysql"],
+    }
+
+    exec { "create-mysql-${name}-user":
+      command => "mysql -u root -p${mysql_root_password} -e \"CREATE USER '${user}'@'localhost' IDENTIFIED BY '${password}'; GRANT ALL PRIVILEGES ON ${name}.* TO '${user}'@'localhost';\"",
+      path    => "/bin:/usr/bin",
+      unless  => "mysql -u${user} -p${password} ${name}",
+      require => Service["mysql"],
+    }
+  }
+  
+}
