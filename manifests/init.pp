689cc6bd7ab619281b1ae364417c181ec77c1ebb
Sam Morrison <sorrison@gmail.com>
2012-05-02 14:43:58 +1000

    my.cnf for precise/lucid

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,16 +16,14 @@ class mysql {
     require => Service["mysql"],
   }
 
-  if $lsbdistcodename == 'precise' {  
-	file { '/etc/mysql/my.cnf':
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      content => template('mysql/my.cnf.erb'),
-      notify  => Service['mysql'],
-      require => Package['mysql-server'],
-    }
+  file { '/etc/mysql/my.cnf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("mysql/my.cnf-${lsbdistcodename}.erb"),
+    notify  => Service['mysql'],
+    require => Package['mysql-server'],
   }
   
   define mysqldb( $user, $password, $access='localhost' ) {
